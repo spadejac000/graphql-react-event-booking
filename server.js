3eba4b98a122acecf28d0bebfca6e0eda34fc710
Jacob Spade <spade.jacob@Yahoo.com>
2019-07-08 11:39:03 -0700

    schemas and resolvers

--- server.js
+++ server.js
@@ -1,13 +1,38 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphQlHttp = require('express-graphql');
+const {buildSchema} = require('graphql')
 
 const app = express();
 
 app.use(bodyParser.json())
 
-app.get('/', (req, res, next) => {
-  res.send('hello world!!!');
-})
+app.use('/graphql', graphQlHttp({
+  schema: buildSchema(`\
+    type RootQuery {
+      events: [String!]!
+    }
+
+    type RootMutation {
+      createEvent(name: String): String
+    }
+
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+  rootValue: {
+    events: () => {
+      return ['Romatic Cooking', 'Sailing', 'All-Night Coding']
+    },
+    createEvent: (args) => {
+      const eventName = args.name;
+      return eventName;
+    }
+  },
+  graphiql: true
+}))
 
 app.listen(5000);
 
